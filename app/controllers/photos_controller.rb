68985e0aa947ba6ed9d365105bbaa7a84eaed1d6
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-04-29 18:56:00 -0500

    Configure flickr API
    Configured flickr gem to search photos of an user.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,5 +4,13 @@
 class PhotosController < ApplicationController
   def index; end
 
-  def search; end
+  def search
+    puts ENV['FLICKR_API_KEY']
+    flickr_api = Flickr.new ENV['FLICKR_API_KEY'], ENV['FLICKR_SHARED_SECRET']
+
+    photos = flickr_api.photos.search user_id: params[:user_id]
+    @user_photos = photos.map { |photo| "https://live.staticflickr.com/#{photo.server}/#{photo.id}_#{photo.secret}.jpg" }
+
+    render :index
+  end
 end
