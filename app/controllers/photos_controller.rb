f9712ef8e74c4f6913062ad0a0fdd3098af03cbd
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-04-29 17:29:48 -0500

    Create controller for flickr photos.

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Flickr photos controller
+class PhotosController < ApplicationController
+  def index; end
+
+  def search; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :photos do
+    get 'search', on: :collection
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
